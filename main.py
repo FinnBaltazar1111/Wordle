5aceac799d45c32df55631a837cecf209ca12515
/script <65987444+FinnBaltazar1111@users.noreply.github.com>
2024-01-12 20:02:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify
-import os
-
+from flask import Flask
+import os,datetime,requests,json
+yr=datetime.datetime.now().year
+mn=datetime.datetime.now().month
+dy=datetime.datetime.now().dy
+url = 'https://www.nytimes.com/svc/wordle/v2/'+yr+'-'+mn+'-'+dy+'.json'
 app = Flask(__name__)
-
-
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
+    return request.get(url)['solution']
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
